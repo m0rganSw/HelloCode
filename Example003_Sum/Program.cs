87eb82e4487eafb89233b2575976cd450c5b1d42
Viktor Sergeyevich Yeliseyev <103144967+m0rganSw@users.noreply.github.com>
2022-08-06 17:17:58 +0700

    add Randome

--- Example003_Sum/Program.cs
+++ Example003_Sum/Program.cs
@@ -1,3 +1,5 @@
-﻿int numberA = 3;
-int numberB = 8;
+﻿int numberA = new Random().Next(1,100);
+Console.WriteLine(numberA);
+int numberB = new Random().Next(1,100);
+Console.WriteLine(numberB);
 Console.WriteLine(numberA + numberB);
