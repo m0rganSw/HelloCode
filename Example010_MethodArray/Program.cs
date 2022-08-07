e8169e46a733bf91943d26874cf66cb603fe4b74
Viktor Sergeyevich Yeliseyev <103144967+m0rganSw@users.noreply.github.com>
2022-08-07 19:21:07 +0700

    add Example 010 Method Array

---
+++ Example010_MethodArray/Program.cs
@@ -0,0 +1,16 @@
+﻿int[] array = {1, 2, 18, 4, 18, 6, 7, 18};
+
+int n = array.Length;
+int find = 18;
+
+int index = 0;
+
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break; // без break - выводит все позиции с числом 18, с break выводит первое искомое.
+    }
+    index++;
+}
