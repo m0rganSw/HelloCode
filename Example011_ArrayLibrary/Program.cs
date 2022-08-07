8dde8bb47e18083bc2891d1fcb9295512b3efeb3
Viktor Sergeyevich Yeliseyev <103144967+m0rganSw@users.noreply.github.com>
2022-08-07 19:38:54 +0700

    add Example 011 Array Library создали три функции. Методы и массивы.

---
+++ Example011_ArrayLibrary/Program.cs
@@ -0,0 +1,52 @@
+﻿void FillArray(int[] collection)
+{
+    int length = collection.Length;
+    int index = 0;
+
+    while (index < length)
+    {
+        collection[index] = new Random().Next(1, 10);
+        index++;
+    }
+}
+
+void PrintArray(int[] collect)
+{
+    int count = collect.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.WriteLine(collect[position]);
+        position++;
+    }
+}
+
+int IndexOF(int[] collection, int find)
+{
+    int count = collection.Length;
+    int index = 0;
+    int position = -1;
+
+    while (index < count)
+    {
+        if (collection[index] == find)
+        { 
+            position = index;
+            break;
+        }
+        index++;
+    }
+    return position;
+}
+
+int[] array = new int[10]; // создай новый массив, в ктором будет 10 эллементов.
+
+FillArray(array);
+PrintArray(array);
+
+Console.WriteLine();
+
+int pos = IndexOF(array, 7);
+Console.WriteLine(pos);
+
+
